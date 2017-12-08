9a40ec81e6f4d1b0cf31a10fe73b4da4f6bf76b8
andrewschultz <schultz.andrew@sbcglobal.net>
2017-12-07 23:44:50 -0600

    basic palindrome extractor fixes

--- utils/1w.py
+++ utils/1w.py
@@ -1,17 +1,28 @@
 import sys
+import re
 from collections import defaultdict
 
 found = defaultdict(int)
 
+pal_list = defaultdict(str)
+
 pals = []
 
 for x in sys.argv[1:]:
-    pals.extend(x.split(","))
-    print(pals)
+    for y in x.split(","):
+        if "V" in y:
+            for z in ['a','e','i','o','u']:
+                temp = re.sub("V", z, y)
+                pals.append(temp)
+        else:
+            pals.append(y)
+
+print(pals)
 
 count = 0
 
-print(pals)
+for x in pals:
+    found[x] = 0
 
 with open("c:/writing/dict/brit-1word.txt") as file:
     for line in file:
@@ -19,12 +30,22 @@ with open("c:/writing/dict/brit-1word.txt") as file:
         for st in pals:
             x = st + l
             if x == x[::-1]:
-                print('FIRST', st, '+' , l, '=', x)
                 found[st] = found[st] + 1
+                if pal_list[st]:
+                    pal_list[st] = pal_list[st] + "\n"
+                pal_list[st] = pal_list[st] + "FIRST {:s} + {:s} = {:s}".format(st, l, x)
+                # print("Added", st, l)
             y = l + st
             if y == y[::-1]:
-                print('LAST', l, '+' , l, '=', y)
+                if pal_list[st]:
+                    pal_list[st] = pal_list[st] + "\n"
+                pal_list[st] = pal_list[st] + "LAST {:s} + {:s} = {:s}".format(st, l, y)
                 found[st] = found[st] + 1
+                # print("Added", l, st)
 
 for x in sorted(found.keys()):
-    print("Found", found[x], "palindromes for", x)
+    if found[x]:
+        print(x, "================")
+        print(pal_list[x])
+    else:
+        print("Nothing found for", x)
