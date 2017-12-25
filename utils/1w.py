bdde5c15e80a7f4b8b0ecf3663e88da2b9b2dcb0
andrewschultz <schultz.andrew@sbcglobal.net>
2017-12-24 20:40:42 -0600

    added usage for 1w.py

--- utils/1w.py
+++ utils/1w.py
@@ -14,11 +14,23 @@ word_cand = defaultdict(bool)
 end_cand = defaultdict(lambda: defaultdict(bool))
 start_cand = defaultdict(lambda: defaultdict(bool))
 
+def usage():
+    print("any word sent in is scoured for palindromes. CSV or space.")
+    print("(Capital) V means try all vowels e.g. bVg = big bag bog bug beg byg")
+    print("-i uses stdin")
+    print("-? shows this usage without telling you you wrote in a bad flag")
+    exit()
+
 def palz(pals):
     for x in pals:
         found[x] = 0
+    loc_end = {}
+    loc_start = {}
     for st in pals:
-        for l in end_cand[st[0]].keys():
+        loc_end[st] = sorted(end_cand[st[0]].keys())
+        loc_start[st] = sorted(start_cand[st[-1]].keys())
+    for st in sorted(pals):
+        for l in loc_end[st]:
             x = st + l
             if x == x[::-1]:
                 found[st] = found[st] + 1
@@ -26,7 +38,7 @@ def palz(pals):
                     pal_list[st] = pal_list[st] + "\n"
                 pal_list[st] = pal_list[st] + "FIRST *{:s}* + {:s} = {:s}".format(st, l, x)
                 # print("Added", st, l)
-        for l in start_cand[st[-1]].keys():
+        for l in loc_start[st]:
             y = l + st
             if y == y[::-1]:
                 if pal_list[st]:
@@ -42,11 +54,17 @@ def palz(pals):
             print("Nothing found for", x)
 
 for x in sys.argv[1:]:
+    xl = x.lower()
     if x == "-i":
         only_stdin = True
+    elif x == "-?" or x == "?":
+        usage()
+    elif not x.replace(",", "").isalpha():
+        print("Invalid flag", -x)
+        usage()
     for y in x.split(","):
         if "V" in y:
-            for z in ['a','e','i','o','u']:
+            for z in ['a','e','i','o','u', 'y']:
                 temp = re.sub("V", z, y)
                 pals.append(temp)
         else:
