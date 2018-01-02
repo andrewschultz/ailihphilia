a435529d2ddf65cd311743eba8718d5ab279072a
andrewschultz <schultz.andrew@sbcglobal.net>
2018-01-01 21:19:35 -0600

    new MSC.PY tool for verifying walkthrough pts = source pts

---
+++ utils/msc.py
@@ -0,0 +1,44 @@
+#
+# msc.py
+#
+# checks that maximum score in the source is in tune with other areas
+#
+
+from collections import defaultdict
+
+import re
+
+mscore = defaultdict(int)
+mokay = defaultdict(int)
+
+with open("c:/games/inform/put-it-up.inform/source/story.ni") as file:
+	for line in file:
+		if 'is a region. max-score of' in line:
+			l2 = re.sub(".* of ", "", line.strip().lower())
+			l2 = re.sub("\.", "", l2)
+			la = l2.split(" is ")
+			mscore[la[0]] = int(la[1])
+
+with open("c:/writing/scripts/invis/pu.txt") as file:
+	for line in file:
+		ll = re.sub("\.", "", line.lower().strip())
+		if ll.startswith("there are") and 'total points in' in ll:
+			ll = re.sub("there are ", "", ll)
+			l2 = ll.split(" total points in ")
+			mokay[l2[1]] = int(l2[0])
+
+print(mokay.keys())
+print(mscore.keys())
+
+mlist = list(set(mokay.keys()) | set(mscore.keys()))
+
+for x in mlist:
+	if x not in mokay.keys():
+		print(x, "not in walkthrough file.")
+	elif x not in mscore.keys():
+		print(x, "not in source file.")
+	elif mscore[x] != mokay[x]:
+		print(x, "has different values:", mscore[x], "in source," , mokay[x], "in walkthrough.")
+
+	else:
+		print(x, "matches source and walkthrough with", mscore[x])
