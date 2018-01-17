3f5a03b1ba96440e761f15616ceac0b1f6613122
andrewschultz <schultz.andrew@sbcglobal.net>
2018-01-16 22:13:48 -0600

    output shuffling for 32.py

--- utils/32.py
+++ utils/32.py
@@ -20,18 +20,19 @@ pals_with_start = defaultdict(str)
 pals_with_end = defaultdict(str)
 got_yet = defaultdict(bool)
 
-min_length = 1
-max_length = 5
+min_length = 2
+max_length = 10
 
 two_word_pals_too = False
+end_word_at_end = False
 
 def chunk(a, b):
     if len(a) > len(b):
         if a[:len(b)] == b[::-1]:
             return (a[len(b):], None)
     elif len(b) > len(a):
-        if b[:len(a)] == a[::-1]:
-            return (b[len(a):], None)
+        if b[-len(a):] == a[::-1]:
+            return (None, b[:-len(a)])
     return(None, None)
 
 t = time.time()
@@ -39,13 +40,12 @@ t = time.time()
 with open("c:/writing/dict/brit-1word.txt") as file:
     for line in file:
         ll = line.lower().strip()
-        if len(ll) < min_length:
-            continue
-        if len(ll) > max_length:
-            continue
-        allword[ll] = True
+        if len(ll) < min_length: continue
+        if len(ll) > max_length: continue
         starts[ll[:2]][ll] = True
         ends[ll[-2:]][ll] = True
+        starts[ll[:1]][ll] = True
+        ends[ll[-1:]][ll] = True
 
 for a in ascii_lowercase:
     for b in ascii_lowercase:
@@ -57,7 +57,7 @@ for a in ascii_lowercase:
                 if not s and not e: continue
                 if s:
                     if s not in pals_with_start.keys():
-                        for w in allword.keys():
+                        for w in ends[s[0]].keys():
                             if (s + w) == (s + w)[::-1]:
                                 pals_with_start[s] = pals_with_start[s] + ' ' + w
                         if not pals_with_start[s]:
@@ -65,7 +65,10 @@ for a in ascii_lowercase:
                     if pals_with_start[s]:
                         if pals_with_start[s][0] == ' ':
                             pals_with_start[s] = pals_with_start[s][1:]
-                        print(x, '/', pals_with_start[s], '/', y)
+                        if end_word_at_end:
+                            print(x, '/', pals_with_start[s], '/', y)
+                        else:
+                            print(x, '/', y, '<=>', pals_with_start[s])
                     continue
                 if e:
                     if e not in pals_with_end.keys():
@@ -77,17 +80,13 @@ for a in ascii_lowercase:
                     if pals_with_end[e]:
                         if pals_with_start[e][0] == ' ':
                             pals_with_start[e] = pals_with_start[e][1:]
-                        print(x, '/', pals_with_end[e], '/', y)
+                        if end_word_at_end:
+                            print(x, '/', pals_with_end[e], '/', y)
+                        else:
+                            print(x, '/', y, '<=>', pals_with_end[e])
                     continue
 
 t = time.time() - t
 
 out = "{:.2f} total seconds taken.".format(t)
 sys.stderr.write(out + '\n')
-
-exit()
-
-check_string = "aid"
-
-print(check_string, pals_with_end[check_string])
-print(check_string, pals_with_start[check_string])
