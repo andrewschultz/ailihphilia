a6afe891c3bee66ef262e525a132779f4859765b
andrewschultz <schultz.andrew@sbcglobal.net>
2018-06-10 13:10:34 -0500

    tweaked walkthrough to mention USE TENET ON BRO ORB substitute, fixed minor revit.py bugs

--- utils/revit.py
+++ utils/revit.py
@@ -11,6 +11,8 @@ what_skipped = defaultdict(str)
 
 orb_next = False
 line_nums = []
+start_low = start_high = 0
+end_low = end_high = 0
 
 def make_wthru(start_num, end_num):
     rev_part = False
@@ -27,22 +29,29 @@ def make_wthru(start_num, end_num):
                 f.write(">rv {:d}\n#skipping point scoring commands {:d} to {:d}\n\n". format(end_num - start_num + 1, start_num, end_num))
             if line_count == line_nums[end_num]:
                 rev_part = False
-            line_mod = re.sub("\(\+1\)", "", line)
+            line_mod = re.sub("\(\+1\) *", "", line)
             if not rev_part: f.write(line_mod)
-            elif rev_part and line_count in line_nums: f.write("#{:s}".format(what_skipped[line_count]))
+            elif rev_part and line_count in line_nums: f.write("#{:s} (skipped)\n\n".format(what_skipped[line_count]))
     f.close()
     print("Wrote", file_name)
 
 with open("walkthrough.txt") as file:
     cmd_count = 0
     for (line_count, line) in enumerate(file, 1):
-        if line.startswith("(+1)"): line_nums.append(line_count)
-        elif '>get all' in line.lower() and orb_next:
+        if line.startswith("(+1)"):
+            line_nums.append(line_count)
+            what_skipped[line_count] = re.sub(".*> *", ">", line.strip())
+        elif '>get all' in line.lower() and orb_next: # this is a hack for getting the orb, which doesn't give a point.
             orb_next = False
             line_nums.append(line_count)
-            what_skipped[line_count] = line
-        elif 'tenet on orb' in line.lower(): orb_next = True
+            what_skipped[line_count] = line.strip()
+        elif 'use tenet on bro orb' in line.lower(): orb_next = True
 
-print(line_nums)
+for x in line_nums: print(x, what_skipped[x])
 
+if start_low > end_high: sys.exit("Lowest start must be less than the highest end.")
+
+for x in range (start_low, start_high + 1):
+    for y in range (end_low, end_high + 1):
+        if x > y: continue
 make_wthru(3, 4)
