e129666fad831bd2948f7ab2ea8e3f7d224df173
andrewschultz <schultz.andrew@sbcglobal.net>
2018-06-14 19:30:32 -0500

    tcount has cmd line

--- utils/tcount.py
+++ utils/tcount.py
@@ -1,12 +1,33 @@
+#
+# tcount.py
+#
+# offers basic metrics on random table size
+#
+
 from collections import defaultdict
 import math
 import re
 import i7
+import sys
+
+biggest_first = True
+verbose = False
 
 table_sizes = defaultdict(int)
 
 in_table = False
 get_header = False
+
+argcount = 1
+
+while argcount < len(sys.argv):
+    arg = sys.argv[argcount]
+    if arg[0] == '-': arg = arg[1:]
+    if arg[0] == 'v': verbose = True
+    elif arg[0] == 'nv': verbose = False
+    elif arg[0] == 'd': biggest_first = True
+    elif arg[0] == 'u': biggest_first = False
+    argcount += 1
 
 with open(i7.tafi('ai')) as file:
     for (line_count, line) in enumerate(file, 1):
@@ -22,7 +43,7 @@ with open(i7.tafi('ai')) as file:
         if in_table and not line.strip():
             in_table = False
             table_sizes[table_name] = line_count - table_start
-            print(table_name, table_sizes[table_name], "rows")
+            if verbose: print(table_name, table_sizes[table_name], "rows")
 
 amean = gmean = gmeanp = 0
 
@@ -34,10 +55,10 @@ with open('tcount.txt') as file:
         if ll.startswith('-'):
             l2 = ll[1:]
             if l2 in table_sizes.keys():
-                print(l2, "discounted.")
+                if verbose: print(l2, "discounted.")
                 table_sizes.pop(l2)
             else:
-                print(l2, "not a valid table key in tcount.txt.")
+                print("Tried to zap <{:s}>, but it's not a valid table key in tcount.txt.".format(l2))
 
 tsize = len(table_sizes.keys())
 
@@ -55,7 +76,7 @@ gmeanp /= tsize
 gmean = math.exp(gmean)
 gmeanp = math.exp(gmeanp)
 
-for x in sorted(table_sizes, key=table_sizes.get, reverse=True):
+for x in sorted(table_sizes, key=table_sizes.get, reverse=biggest_first):
     gmtweak = gmean * ((table_sizes[x] + 1) / table_sizes[x]) ** (1/tsize)
     count += 1
     print(count, x, table_sizes[x], gmtweak)
