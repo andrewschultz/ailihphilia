30187b09e230c7c15069e64d33e31a21c2c8d417
andrewschultz <schultz.andrew@sbcglobal.net>
2018-06-29 11:43:17 -0500

    utw updated for user friendliness

--- utils/utw.py
+++ utils/utw.py
@@ -3,11 +3,13 @@
 # takes the table of specific rejects and goes through each combination
 #
 
+import i7
+import sys
 import re
 from collections import defaultdict
 
-in_source = defaultdict(bool)
-in_unit = defaultdict(bool)
+in_source = defaultdict(int)
+in_unit = defaultdict(int)
 
 figure_out = True
 compfile = "reg-ail-use-unit-tests.txt"
@@ -15,36 +17,58 @@ compfile = "reg-ail-use-unit-tests.txt"
 in_use_table = False
 header_next = False
 
+open_line_source = 0
+open_line_testfile = 0
+open_after = False
+
 writestr = ""
 
-au3 = open("utw.au3", "w")
-
-au3_header = "Opt(\"SendKeyDelay\", 0)\n\nOpt(\"WinTitleMatchMode\", -2)\nWinActivate(\"Ailihphilia.inform \")\nWinWaitActive(\"Ailihphilia.inform \")\n\nMouseMove(1640,972)\n\nSleep(500)\n\n";
-
-au3.write(au3_header)
-
 def compare_unit_and_source():
+    global open_line_source
+    global open_line_testfile
     print('Comparing story.ni with', compfile)
     eu = 0
     mu = 0
     with open(compfile) as file:
-        for line in file:
+        for (line_count, line) in enumerate(file, 1):
             if line.startswith('>uu'):
-                in_unit[line.strip().lower()] = True
+                in_unit[line.strip().lower()] = line_count
     ins = list(set(in_unit.keys()) | set(in_source.keys()))
     for x in ins:
         if x not in in_source.keys():
-            print("Errant unit test", x)
+            print("(line {:05d}) Errant unit test".format(in_unit[x]), x)
+            if not open_line_testfile: open_line_testfile = in_unit[x]
             eu += 1
     for x in ins:
         if x not in in_unit.keys():
-            print("Missing unit test", x)
+            print("(line {:05d}) Missing unit test for command".format(in_source[x]), x)
+            if not open_line_source: open_line_source = in_source[x]
             mu += 1
     print(eu, 'errant')
     print(mu, 'missing')
 
+count = 1
+while count < len(sys.argv):
+    arg = sys.argv[count].lower()
+    if arg[0] == '-': arg = arg[1:]
+    if arg == 'l': open_after = True
+    elif arg == 'nl' or arg == 'ln': open_after = False
+    elif arg == 'nf' or arg == 'fn': figure_out = False
+    elif arg == 'nw' or arg == 'wn': figure_out = True
+    elif arg == 'f': figure_out = True
+    elif arg == 'w': figure_out = False
+    else:
+        print("Invalid option", arg)
+        sys.exit("There are only 2 options right now:\n-f/-w to figure or write out\n-l opens the first wrong line, or -nl/-ln disables this. Default is currently {:s}.".format(str(i7.oo[open_after])))
+    count += 1
+
+if not figure_out:
+    au3 = open("utw.au3", "w")
+    au3_header = "Opt(\"SendKeyDelay\", 0)\n\nOpt(\"WinTitleMatchMode\", -2)\nWinActivate(\"Ailihphilia.inform \")\nWinWaitActive(\"Ailihphilia.inform \")\n\nMouseMove(1640,972)\n\nSleep(500)\n\n";
+    au3.write(au3_header)
+
 with open("story.ni") as file:
-    for line in file:
+    for (line_count, line) in enumerate(file, 1):
         if header_next:
             in_use_table = True
             header_next = False
@@ -60,7 +84,7 @@ with open("story.ni") as file:
             if la[0] == '--': continue
             cmd = ">uu {:s} on {:s}".format(la[0].lower(), la[1].lower())
             if figure_out:
-                in_source[cmd] = True
+                in_source[cmd] = line_count
                 continue
             writestr += "{:s}\n".format(cmd)
             writestr += "{:s}\n\n".format(actual_text)
@@ -74,6 +98,15 @@ au3.close()
 if figure_out:
     compare_unit_and_source()
 else:
-    reg = open("raw-ail-use-unit-tests.txt", "w")
+    print("Writing to raw-ai-use-unit-tests.txt")
+    reg = open("raw-ai-use-unit-tests.txt", "w")
     reg.write(writestr)
     reg.close()
+
+if open_after:
+    if not open_line_source and not open_line_testfile: print("No errors, so I'm not opening the source to find one.")
+    else:
+        if open_line_source: i7.npo(i7.src("ai"), open_line_source, bail=False)
+        if open_line_testfile: i7.npo(compfile, open_line_testfile, bail=False)
+else:
+    if open_line_source or open_line_testfile: print("-l will launch the file(s) at the first errant line.")
