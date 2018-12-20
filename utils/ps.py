22d13396be53c5f8020a84dd6a986edfdd624a3c
andrewschultz <schultz.andrew@sbcglobal.net>
2018-12-20 13:29:07 -0600

    ps.py tweaks

--- utils/ps.py
+++ utils/ps.py
@@ -1,28 +1,42 @@
+# ps.py
+#
+# palindrome search: Looks for (A) + letter + (A reverse) or (A reverse) + letter + (A) 
+#
+
 import re
 import os
 import sys
+
+print_finds_and_starts = False
 
 q = [x for x in sys.argv[1:] if re.search("^[a-z]+$", x.lower())]
 
 if len(q) == 0: sys.exit("You need to put palindrome-halves in the command line.")
 
-finds = r'({:s})'.format("|".join([r + "." + r[::-1] for r in q]))
+finds = r'({:s})'.format("|".join([r + "." + r[::-1] + "|" + r[::-1] + "." + r for r in q]))
 start = r'\b({:s})'.format("|".join(q))
 
-print(start)
+if print_finds_and_starts:
+    print("finds", finds)
+    print("start", start)
+    exit()
 
 def find_one(file_name):
-	print(file_name)
-	with open(file_name) as file:
-		for (line_count, line) in enumerate(file, 1):
-			l = line.lower().strip()
-			lc = re.sub("[^a-z]", "", l)
-			if re.search(finds, lc):
-				j = []
-				for q2 in q:
-					if re.search('\b{:s}'.format(q2), l):
-						j.append(q2)
-				q3 = "<" + "/".join(j) + ">" if len(j) else "<split word>"
-				print(line_count, q3, line.strip())
+    print(file_name)
+    count = 0
+    with open(file_name) as file:
+        for (line_count, line) in enumerate(file, 1):
+            l = line.lower().strip()
+            lc = re.sub("[^a-z]", "", l)
+            if re.search(finds, lc):
+                j = []
+                k = []
+                for q2 in q:
+                    if re.search(r'\b{:s}\b'.format(q2), l):
+                        j.append(q2)
+                    if q2 in lc: k.append(q2)
+                q3 = "<{:s}{:s}>".format("split word " if len(j) == 0 else "", '/'.join(k))
+                count += 1
+                print("{:5d}:{:3d}".format(line_count, count), q3, line.strip())
 
 find_one("story.ni")
