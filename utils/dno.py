24376233b906b7cc27889ade3c05f397bbd1d094
andrewschultz <schultz.andrew@sbcglobal.net>
2019-07-11 19:57:01 -0500

    dno.py now adds smart ideas

--- utils/dno.py
+++ utils/dno.py
@@ -17,11 +17,13 @@ from shutil import copy
 pals = defaultdict(int)
 twice = defaultdict(bool)
 
+pro="ailihphilia"
+
 # constants
 on_off = [ "off", "on" ]
 
 # options
-copy_double_comments = False
+display_changes = False
 notes_file_order = False
 notes_file_reverse = False
 modify_notes = True
@@ -42,24 +44,137 @@ list_sections = False
 do_detail = False
 compare_temp = False
 
+copy_smart = False
+copy_to_old = False
+
 notes_array = []
 
 # variables
 colon_string = ""
 
-def copy_to_backup(pro):
+def unique_header(a, b):
+    for x in b:
+        if a == x: return x
+        if 'xx' + a == x: return x
+    temp_ret = ""
+    for x in b:
+        if x.startswith(a) or x.startswith('xx' + a):
+            if temp_ret:
+                print("Uh oh, {0} is ambiguous. It could be {1} or {2}.")
+                return ""
+            temp_ret = x
+    return temp_ret
+
+def show_nonblanks(file_name):
+    total = comments = comments_to_shift = blanks = ideas = 0
+    with open(file_name) as file:
+        for (line_count, line) in enumerate(file, 1):
+            total += 1
+            if line.startswith("##"): comments_to_shift += 1
+            elif line.startswith("#"): comments += 1
+            elif not line.strip(): blanks += 1
+            else: ideas += 1
+    print("Ideas:", ideas, "Comments to shift:", comments_to_shift, "Comments:", comments, "Blanks:", blanks, "Total:", total)
+
+def copy_smart_ideas(pro, hdr_type = "ta"):
+    notes_in = os.path.join(i7.proj2dir(pro), "notes.txt")
+    notes_out = os.path.join(i7.proj2dir(pro), "notes-temp.txt")
+    hdr_tmp = os.path.join(i7.extdir, "temp.i7x")
+    hdr_to_change = i7.hdr(pro, hdr_type)
+    markers = defaultdict(int)
+    bail = False
+    to_insert = defaultdict(str)
+    with open(hdr_to_change) as file:
+        for (line_count, line) in enumerate(file, 1):
+            if line.startswith("table of") and "[" in line and "\t" not in line:
+                x = re.findall("\[([^\]]*)\]", line)
+                for y in x:
+                    y = re.sub("[\[\]]", "", y)
+                    if y in markers:
+                        bail = True
+                        print("Uh oh double definition of {0}, lines {1} and {2}.".format(y, markers[y], line_count))
+                    else:
+                        markers[y] = line_count
+    if bail: sys.exit()
+    msort = sorted(markers)
+    for y in range(0, len(msort)):
+        for z in range(y+1, len(msort)):
+            n1 = msort[y]
+            n2 = msort[z]
+            if n1.startswith(n2) or n2.startswith(n1):
+                bail = True
+                print("Uh oh overlapping {0} {1} {2} {3}.".format(n1, markers[n1], n2, markers[n2]))
+    if bail: sys.exit()
+    out_stream = open(notes_out, "w")
+    uniques = 0
+    with open(notes_in) as file:
+        for (line_count, line) in enumerate(file, 1):
+            print_this_line = True
+            if re.search("^[a-zA-Z0-9]+:", line):
+                left_bit = re.sub(":.*", "", line.lower().strip())
+                uh = unique_header(left_bit, markers)
+                if uh:
+                    new_text = re.sub("^[a-zA-Z]+:", "", line.rstrip())
+                    if not new_text.startswith("\""): new_text = "\"" + new_text
+                    if not new_text.endswith("\""): new_text = new_text + "\""
+                    print_this_line = False
+                    to_insert[uh] += new_text + "\n"
+                    uniques += 1
+                else:
+                    if "#idea" not in line.lower():
+                        print("Unrecognized colon starting with", left_bit, "at line", line_count, "full(er) text", line.strip()[:50])
+                # print("Looking for uniques in {0}({1}): {2}".format(line.strip(), uh, print_this_line))
+            if print_this_line: out_stream.write(line)
+    out_stream.close()
+    print(uniques, "uniques found to send to header.")
+    print_after_next = False
+    out_stream = open(hdr_tmp, "w", newline="\n")
+    last_blank = False
+    with open(hdr_to_change) as file:
+        for (line_count, line) in enumerate(file, 1):
+            if last_blank and not line.strip(): continue
+            out_stream.write(line)
+            last_blank = not line.strip()
+            if line.startswith("table of") and "\t" not in line:
+                x = re.findall("\[([^\]]*)\]", line)
+                print_after_next = True
+                continue
+            if print_after_next:
+                print_after_next = False
+                if len(x):
+                    for j in x:
+                        if j in to_insert:
+                            out_stream.write(to_insert[j])
+                            to_insert.pop(j)
+    out_stream.close()
+    if len(to_insert):
+        print("Uh oh! We didn't clear everything.", to_insert)
+    if display_changes:
+        i7.wm(notes_in, notes_out)
+        i7.wm(hdr_to_change, hdr_tmp)
+    else:
+        copy(notes_out, notes_in)
+        copy(hdr_tmp, hdr_to_change)
+    os.remove(notes_out)
+    os.remove(hdr_tmp)
+    if display_changes: sys.exit()
+
+def move_old_ideas(pro):
     notes_in = os.path.join(i7.proj2dir(pro), "notes.txt")
     notes_out = os.path.join(i7.proj2dir(pro), "notes-old.txt")
     notes_temp = os.path.join(i7.proj2dir(pro), "notes-temp.txt")
     no = open(notes_out, "a")
     nt = open(notes_temp, "w")
     got_one = 0
+    blanks = 0
     with open(notes_in) as file:
         for (line_count, line) in enumerate(file, 1):
+            if not line.strip: blanks += 1
             if line.startswith("##"):
                 got_one += 1
                 no.write(line)
             else: nt.write(line)
+    print(blanks, "of", line_count, "total blank lines.")
     no.close()
     nt.close()
     if got_one:
@@ -342,8 +457,13 @@ while count < len(sys.argv):
         twice_okay = False
     if l == '2' or l == '-2':
         twice_okay = True
-    elif l == 'cc':
-        copy_double_comments = True
+    elif l == 'co':
+        copy_to_old = True
+    elif l == 'cs' or l == 'sc':
+        copy_smart = True
+    elif l == 'ca' or l == 'ac' or l == 'bc' or l == 'cb':
+        copy_smart = True
+        copy_to_old = True
     elif l == 'l':
         launch_after = True
     elif l == 'ln' or l == 'nl':
@@ -362,6 +482,8 @@ while count < len(sys.argv):
         verbose = True
     elif l == 'd':
         do_detail = True
+    elif l == 'dc':
+        display_changes = True
     elif l == 'f':
         open_first = True
     elif l == 'ls' or l == 'sl':
@@ -413,8 +535,11 @@ while count < len(sys.argv):
         usage()
     count += 1
 
-if copy_double_comments:
-    copy_to_backup("ailihphilia")
+if copy_smart or copy_to_old:
+    print(copy_smart, copy_to_old)
+    if copy_smart: copy_smart_ideas(pro)
+    if copy_to_old: move_old_ideas(pro)
+    show_nonblanks(os.path.join(i7.proj2dir(pro), "notes.txt"))
     sys.exit()
 
 if do_detail:
